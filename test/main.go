1155c929cdb8ee82c9907bca180bd0058a87029c
hverby <hansbegou@gmail.com>
2019-06-10 06:52:45 +0200

    test

---
+++ test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import piscine ".."
+
+
+
+func main()  {
+	piscine.IsNegative(1)
+	piscine.IsNegative(0)
+	piscine.IsNegative(-1)
+}
---
+++ test/piscine/isnegative_test.go
@@ -0,0 +1,11 @@
+package piscine
+
+import "fmt"
+
+func IsNegative(nb int) {
+	if nb >= 0{
+		fmt.Println("F")
+	}else{
+		fmt.Println("T")
+	}
+}
