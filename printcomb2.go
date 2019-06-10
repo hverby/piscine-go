9818d350411b33c81dac11b60894537cbbade3f7
hverby <36340605+hverby@users.noreply.github.com>
2019-06-10 07:36:33 +0200

    Update printcomb2.go

--- printcomb2.go
+++ printcomb2.go
@@ -4,15 +4,17 @@ import "fmt"
 import "strconv"
 
 func PrintComb2()  {
-	for i:=0;i<10;i++{
-		for j:=0;j<10;j++{
-			for k:=0;k<10;k++{
+	for x:=0;x<10;x++{
+		for y:=0;y<10;y++{
+			for z:=0;z<10;z++{
 				for l:=0;l<10;l++{
-					nb1:= strconv.Itoa(i) + strconv.Itoa(j)
-					nb2:= strconv.Itoa(k) + strconv.Itoa(l)
-					if(nb1 < nb2){
-						fmt.Print(nb1 + " " +nb2)
-						if nb1 < "98" {
+					nbr1:= strconv.Itoa(x) + strconv.Itoa(y)
+					nbr2:= strconv.Itoa(z) + strconv.Itoa(l)
+					if(nbr1 < nbr2){
+						fmt.Print(nbr1 + " " +nbr2)
+						
+						
+						if nbr1 < "98" {
 							fmt.Print(", ")
 						}else {
 							fmt.Print("\n")
