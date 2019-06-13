3538b2810431d2d84dbdf54e795ef435d6cd4bf7
hverby <36340605+hverby@users.noreply.github.com>
2019-06-13 19:36:48 +0100

    maj

--- sqrt.go
+++ sqrt.go
@@ -1,9 +1,12 @@
 package piscine
 
-func IsPrime(nb int) bool {
-	if nb%2 == 0 {
-		return true
-	}
+import "math"
 
-	return false
+func Sqrt(nb int) int {
+	if nb%2 == 0 {
+		x := math.Sqrt(float64(nb))
+		return int(x)
+	}
+	return 0
+
 }
