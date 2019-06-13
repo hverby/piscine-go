ec5227eef9949f94a59803bc9bf59bffddaa8221
hverby <36340605+hverby@users.noreply.github.com>
2019-06-13 13:54:03 +0100

    lalala

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,21 @@
-package piscine
+package main
 
-func IterativeFactorial(nb int) int {
-	result := 1
-	for i := 1; i <= nb; i++ {
-		if nb > 0 {
-			result *= i
+import "math"
+
+func IterativeFactorial(x int) int {
+	if 0 > x {
+		return 0
+	} else if 0 == x || 1 == x {
+		return 1
+	} else {
+		res := 1
+		for i := 1; i <= x; i++ {
+			res *= i
+			if math.MaxInt32 < res {
+				res = 0
+				break
+			}
 		}
+		return res
 	}
-	return result
 }
