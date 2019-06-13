25b9858119d852974ea9100cb9171611c340ebaa
hverby <36340605+hverby@users.noreply.github.com>
2019-06-13 15:56:31 +0100

    maj

--- iterativepower.go
+++ iterativepower.go
@@ -4,11 +4,14 @@ import (
 	"math"
 )
 
-func IterativePower(nb int, power int) int {
-	x, y := float64(nb), float64(power)
-	res := math.Pow(x, y)
-	if res < 0 {
+func RecursivePower(nb int, power int) int {
+
+	if power < 0 {
 		return 0
 	}
+	x, y := float64(nb), float64(power)
+	res := math.Pow(x, y)
+
 	return int(res)
+
 }
