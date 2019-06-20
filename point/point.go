9af1581f2f6bf16bcc0ceab40ae81408332a0e4f
hverby <36340605+hverby@users.noreply.github.com>
2019-06-20 13:22:59 +0100

    Create point.go

---
+++ point/point.go
@@ -0,0 +1,22 @@
+package point
+
+import "fmt"
+
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	fmt.Printf("x = %d, y = %d\n",points.x, points.y)
+}
