1e9d8314dcffe22f7d7d7cdec52c4b2112c4c171
hverby <36340605+hverby@users.noreply.github.com>
2019-06-21 21:05:57 +0100

    Update main.go

--- doop/main.go
+++ doop/main.go
@@ -20,36 +20,38 @@ func main(){
 	args := os.Args[1:]
 	if len(args) != 3 {
 
-	}
-	if isSigne(args[1], signe) && len(args) == 3{
-		nb1, err := strconv.Atoi(args[0])
-		nb2, err2 := strconv.Atoi(args[2])
-		if err == nil && err2 == nil{
-			switch args[1] {
-			case "+":
-				fmt.Println(nb1 + nb2)
-			case "-":
-				fmt.Println(nb1 - nb2)
-			case "/":
-				if nb2 == 0 {
-					fmt.Println("No division by 0")
-				}else{
-					fmt.Println(nb1 / nb2)
-				}
-			case "%":
-				if nb2 == 0 {
-					fmt.Println("No Modulo by 0")
-				}else{
-					fmt.Println(nb1 % nb2)
-				}
-			case "*":
-				fmt.Println(nb1 * nb2)
+	}else{
+		if isSigne(args[1], signe){
+			nb1, err := strconv.Atoi(args[0])
+			nb2, err2 := strconv.Atoi(args[2])
+			if err == nil && err2 == nil{
+				switch args[1] {
+				case "+":
+					fmt.Println(nb1 + nb2)
+				case "-":
+					fmt.Println(nb1 - nb2)
+				case "/":
+					if nb2 == 0 {
+						fmt.Println("No division by 0")
+					}else{
+						fmt.Println(nb1 / nb2)
+					}
+				case "%":
+					if nb2 == 0 {
+						fmt.Println("No Modulo by 0")
+					}else{
+						fmt.Println(nb1 % nb2)
+					}
+				case "*":
+					fmt.Println(nb1 * nb2)
 
+				}
+			}else{
+				fmt.Println("1")
 			}
 		}else{
-			fmt.Println("1")
+			fmt.Println("0")
 		}
-	}else{
-		fmt.Println("0")
 	}
+	
 }
