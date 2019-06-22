574d010ab952d0fa684d5d7ae9f4491647f12d96
hverby <36340605+hverby@users.noreply.github.com>
2019-06-22 16:17:49 +0100

    Create rot14.go

---
+++ rot14.go
@@ -0,0 +1,58 @@
+package piscine
+
+import (
+	"strings"
+	"unicode"
+)
+
+func Rot14(str string) string {
+	fact := 14
+	res := ""
+	for _,v := range str{
+		if string(v) == " "{
+			res += " "
+		}else{
+			if unicode.IsLower(v) {
+				codeA := 'a'
+				codeL := 'l'
+				codeZ := 'z'
+				if v >= codeA && v <= codeZ {
+					if v > codeL {
+						nb1 := v + int32(fact)
+						if nb1 == 123{
+							res += strings.ToLower(string(codeA))
+						}else{
+							nb2 := nb1 - codeZ
+							nb2 = (codeA + nb2) - 1
+							res += strings.ToLower(string(nb2))
+						}
+					}else{
+						res += strings.ToLower(string(v + 14))
+					}
+				}
+			}else{
+				codeA := 'A'
+				codeL := 'L'
+				codeZ := 'Z'
+				if v >= codeA && v <= codeZ {
+					if v > codeL {
+						nb1 := v + int32(fact)
+						if nb1 == 123{
+							res += strings.ToUpper(string(codeA))
+						}else{
+							nb2 := nb1 - codeZ
+							nb2 = (codeA + nb2) - 1
+							res += strings.ToUpper(string(nb2))
+						}
+					}else{
+						res += strings.ToUpper(string(v + 14))
+					}
+				}
+			}
+
+		}
+
+	}
+	return res
+}
+
