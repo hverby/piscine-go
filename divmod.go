8bb0a9b62e5d1ddd159631ba1f7408f37fe5d877
hverby <36340605+hverby@users.noreply.github.com>
2019-06-22 18:17:11 +0100

    Update divmod.go

--- divmod.go
+++ divmod.go
@@ -1,6 +1,7 @@
 package piscine
 
-func DivMod(a, int, b int,enerveur int ,div *int, mod *int) {
-	*div = a / b
-	*mod = a % b
+
+func DivMod(a int, b int, div *int, mod *int){
+	*div= int(a/b)
+	*mod= a%b
 }
