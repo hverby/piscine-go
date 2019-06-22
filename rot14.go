f384aa7661f1d537d6900a89301d661e5173ce84
hverby <36340605+hverby@users.noreply.github.com>
2019-06-22 16:39:05 +0100

    Update rot14.go

--- rot14.go
+++ rot14.go
@@ -14,43 +14,43 @@ func Rot14(str string) string {
 
 		if string(v) == " "{
 			res += " "
-		}else if(re.MatchString(string(v))){
-
-		} else{
-			if unicode.IsLower(v) {
-				codeA := 'a'
-				codeL := 'l'
-				codeZ := 'z'
-				if v >= codeA && v <= codeZ {
-					if v > codeL {
-						nb1 := v + int32(fact)
-						if nb1 == 123{
-							res += strings.ToLower(string(codeA))
+		}else{
+			if !re.MatchString(string(v)){
+				if unicode.IsLower(v) {
+					codeA := 'a'
+					codeL := 'l'
+					codeZ := 'z'
+					if v >= codeA && v <= codeZ {
+						if v > codeL {
+							nb1 := v + int32(fact)
+							if nb1 == 123{
+								res += strings.ToLower(string(codeA))
+							}else{
+								nb2 := nb1 - codeZ
+								nb2 = (codeA + nb2) - 1
+								res += strings.ToLower(string(nb2))
+							}
 						}else{
-							nb2 := nb1 - codeZ
-							nb2 = (codeA + nb2) - 1
-							res += strings.ToLower(string(nb2))
+							res += strings.ToLower(string(v + 14))
 						}
-					}else{
-						res += strings.ToLower(string(v + 14))
 					}
-				}
-			}else{
-				codeA := 'A'
-				codeL := 'L'
-				codeZ := 'Z'
-				if v >= codeA && v <= codeZ {
-					if v > codeL {
-						nb1 := v + int32(fact)
-						if nb1 == 123{
-							res += strings.ToUpper(string(codeA))
+				}else{
+					codeA := 'A'
+					codeL := 'L'
+					codeZ := 'Z'
+					if v >= codeA && v <= codeZ {
+						if v > codeL {
+							nb1 := v + int32(fact)
+							if nb1 == 123{
+								res += strings.ToUpper(string(codeA))
+							}else{
+								nb2 := nb1 - codeZ
+								nb2 = (codeA + nb2) - 1
+								res += strings.ToUpper(string(nb2))
+							}
 						}else{
-							nb2 := nb1 - codeZ
-							nb2 = (codeA + nb2) - 1
-							res += strings.ToUpper(string(nb2))
+							res += strings.ToUpper(string(v + 14))
 						}
-					}else{
-						res += strings.ToUpper(string(v + 14))
 					}
 				}
 			}
