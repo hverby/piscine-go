2c3e0305668484e9f80d3e55166beabebcc53214
hverby <36340605+hverby@users.noreply.github.com>
2019-06-22 18:05:57 +0100

    Create compact.go

---
+++ compact.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Compact(ptr *[]string, length int) int {
+	count:=0
+	for i:=0;i<length;i++{
+		if (*ptr)[i]!=" "{
+			count++
+		}
+	}
+	return count
+}
+© 2019 GitHub, Inc.
