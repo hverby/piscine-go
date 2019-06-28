eca6e1c596da34922788adc0bacdc5ddd4861990
hverby <36340605+hverby@users.noreply.github.com>
2019-06-28 15:30:20 +0100

    Update listmerge.go

--- listmerge.go
+++ listmerge.go
@@ -1,16 +1,15 @@
 package piscine
 
-//merges the 2 lists in one(in the end of the first list)
 func ListMerge(l1 *List, l2 *List) {
-	iterator := l1.Head
-	iterator2 := l2.Head
-
-	for iterator != nil {
-		if iterator.Next == nil {
-			iterator.Next = iterator2
-			return
-		}
-		iterator = iterator.Next
+	if l2 == nil || l1 == nil {
+		return
 	}
 
+	if l1.Head == nil {
+		l1.Head = l2.Head
+		l1.Tail = l2.Head
+		return
+	}
+
+	l1.Tail.Next = l2.Head
 }
