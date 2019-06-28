93c5a3b63ddfc96f609822dcfac49d0c745874f2
hverby <36340605+hverby@users.noreply.github.com>
2019-06-28 21:43:47 +0100

    Create btreeinsertdata.go

---
+++ btreeinsertdata.go
@@ -0,0 +1,19 @@
+package piscine 
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                 string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+if root == nil{
+		return &TreeNode{Data:data}
+	}
+
+	if data < root.Data{
+		root.Left = BTreeInsertData(root.Left, data)
+	}else{
+		root.Right = BTreeInsertData(root.Right, data)
+	}
+	return root
+}
