6f7b9a3d92a431cf1e594b17419a31dafb95c2f9
hverby <36340605+hverby@users.noreply.github.com>
2019-06-28 03:27:27 +0100

    Update listreverse.go

--- listreverse.go
+++ listreverse.go
@@ -2,15 +2,14 @@ package piscine
 
 func ListReverse(l *List) {
 	current := l.Head
-	var prev *NodeL
-
+	prev := l.Head
+	prev = nil
 	for current != nil {
 		next := current.Next
 		current.Next = prev
 		prev = current
 		current = next
 	}
-
 	temp := l.Head
 	l.Head = l.Tail
 	l.Tail = temp
