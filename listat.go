a3b73d891082ffc1266b85d3a9a62a5484fec32a
hverby <36340605+hverby@users.noreply.github.com>
2019-06-28 02:37:07 +0100

    Create listat.go

---
+++ listat.go
@@ -0,0 +1,22 @@
+package piscine
+
+func ListAt(l *NodeL, nbr int) *NodeL {
+	index := 0
+	count := 0
+	head := l
+
+	for head != nil {
+		index++
+		head = head.Next
+	}
+	if nbr <= index {
+		for l != nil {
+			if count == nbr {
+				return l
+			}
+			count++
+			l = l.Next
+		}
+	}
+	return nil
+}
