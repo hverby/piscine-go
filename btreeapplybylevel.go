548c115b3272f5146c7cca1125b01d69a55600fa
hverby <36340605+hverby@users.noreply.github.com>
2019-06-29 16:58:22 +0000

    Update btreeapplybylevel.go

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,12 +1,31 @@
 package piscine
 
-import "fmt"
+import "reflect"
 
-func BTreeApplyByLevel(root *TreeNode, fn interface{})  {
-if root == nil {
-		return
-	}
-	fmt.Println(fn.Data)
-	BTreeApplyPreorder(root.Left, fn)
-	BTreeApplyPreorder(root.Right, fn)
+func printOrderTraversal(root *TreeNode, niveau int, fn interface{})  {
+        if root == nil { 
+                return 
+        }
+        if niveau == 1 {
+		Invoke(fn, root.Data)
+        }else if niveau > 1 { 
+                printOrderTraversal(root.Left,niveau - 1, fn)
+                printOrderTraversal(root.Right,niveau - 1, fn)
+        }
+}
+
+func BTreeApplyByLevel(root *TreeNode, fn interface{}) {
+        h := BTreeLevelCount(root)
+        for i := 1; i <= h; i++ { 
+                printOrderTraversal(root, i,fn)
+        }
+}
+
+func Invoke(fn interface{}, args ...string) {
+    v := reflect.ValueOf(fn)
+    rargs := make([]reflect.Value, len(args))
+    for i, a := range args {
+        rargs[i] = reflect.ValueOf(a)
+    }
+    v.Call(rargs)
 }
