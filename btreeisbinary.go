096a46eb0f1deeb7e185108a1e155249398e8519
hverby <36340605+hverby@users.noreply.github.com>
2019-06-29 17:05:47 +0100

    Update btreeisbinary.go

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -1,18 +1,23 @@
 package piscine
 
-func BTreeIsBinary(root *TreeNode) bool {
-	if root == nil {
-		return true
-	}
-	if BTreeIsBinary(root.Left) && root.Data > root.Left.Data {
-		return false
-	}
-	if BTreeIsBinary(root.Right) && root.Data < root.Right.Data {
-		return false
-	}
+func BTreeIsBinary(root TreeNode) bool {
+    if root == nil {
+        return true
+    }
 
-	if !BTreeIsBinary(root.Left) || BTreeIsBinary(root.Right){
-		return false
-	}
-	return true
+    / false if left is > than node /
+    if root.Left != nil && root.Left.Data > root.Data {
+        return false
+    }
+    / false if right is < than node /
+    if root.Right != nil && root.Right.Data < root.Data {
+        return false
+    }
+    / false if, recursively, the left or right is not a BST /
+    if !BTreeIsBinary(root.Left) || !BTreeIsBinary(root.Right) {
+        return false
+    }
+    / passing all that, it's a BST */
+    return true
+
 }
