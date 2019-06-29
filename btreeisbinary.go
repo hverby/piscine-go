97b8ddfce6cd61a70852c7b6c019c3a9674031ab
hverby <36340605+hverby@users.noreply.github.com>
2019-06-29 17:07:27 +0100

    Update btreeisbinary.go

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -5,19 +5,16 @@ func BTreeIsBinary(root TreeNode) bool {
         return true
     }
 
-    / false if left is > than node /
+ 
     if root.Left != nil && root.Left.Data > root.Data {
         return false
     }
-    / false if right is < than node /
     if root.Right != nil && root.Right.Data < root.Data {
         return false
     }
-    / false if, recursively, the left or right is not a BST /
     if !BTreeIsBinary(root.Left) || !BTreeIsBinary(root.Right) {
         return false
     }
-    / passing all that, it's a BST */
     return true
 
 }
